7e67a2ace462cf23f88445b1cedfb19f4de14042
JK Wood <joshuakwood@gmail.com>
2011-05-29 04:16:02 -0500

    greppkg.rb - Same as greppkg, only this one behaves itself.

---
+++ greppkg.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/ruby
+# Script to search Slackware packages
+# for text - filenames being the most
+# intuitive use.
+
+# Written by JK Wood
+# joshuakwood@gmail.com
+
+# The nice thing about Ruby is, if the class doesn't contain
+# the method you want, you just write it and move on.
+class File
+	def include? pattern
+		found_pattern = false
+		each_line do |line|
+			if line.include? pattern
+				found_pattern = true
+			end
+		end
+		found_pattern
+	end
+end
+
+# Every script should have a usage statement.
+def usage
+	puts "\nScript for finding patterns in packages"
+	puts "\nUsage: #{$0} DESIRED_FILE\n\n"
+	exit
+end
+
+# Ensure we're being used correctly.
+if ARGF.argv.length == 0 or ARGF.argv[0].eql? "-h"
+	usage
+end
+
+# I will probably clean this up. Someday.
+origDir = Dir.pwd
+foundMatch = false
+Dir.chdir "/var/log/packages/"
+Dir.foreach("/var/log/packages") do |filename|
+	if !File.directory? filename
+		file = File.open filename
+		if file.include? ARGF.argv[0]
+			puts "In #{filename}:"
+			foundMatch = true
+		end
+		file.rewind
+		file.each_line do |line|
+			if line.include? ARGF.argv[0]
+				puts "     #{line}"
+			end
+		end
+		file.close
+	end
+end
+
+Dir.chdir origDir
+
+if !foundMatch
+	puts "No packages contained that pattern."
+end
